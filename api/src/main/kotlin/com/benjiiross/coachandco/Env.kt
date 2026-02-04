11bed6ed35805c183eb087c4b5494788644cafe4
Benjamin <60489646+benjiiross@users.noreply.github.com>
2026-02-04 14:03:09 +0100

    add env variables

---
+++ api/src/main/kotlin/com/benjiiross/coachandco/Env.kt
@@ -0,0 +1,11 @@
+package com.benjiiross.coachandco
+
+object Env {
+  val dbUrl = getEnvOrThrow("DB_URL")
+  val dbUser = getEnvOrThrow("DB_USERNAME")
+  val dbPassword = getEnvOrThrow("DB_PASSWORD")
+  val jwtSecret = getEnvOrThrow("JWT_SECRET")
+
+  private fun getEnvOrThrow(name: String) =
+      System.getenv(name) ?: error("CRITICAL ERROR: Missing env variable [$name].")
+}
