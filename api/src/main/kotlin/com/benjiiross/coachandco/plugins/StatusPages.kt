21482e6348c1d9fff4ee4bed97e60d52e220e762
Benjamin <60489646+benjiiross@users.noreply.github.com>
2026-02-05 18:11:35 +0100

    add userMapper & status pages

---
+++ api/src/main/kotlin/com/benjiiross/coachandco/plugins/StatusPages.kt
@@ -0,0 +1,28 @@
+package com.benjiiross.coachandco.plugins
+
+import com.benjiiross.coachandco.core.exceptions.InvalidIdException
+import com.benjiiross.coachandco.core.exceptions.ResourceNotFoundException
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.application.Application
+import io.ktor.server.application.install
+import io.ktor.server.plugins.statuspages.StatusPages
+import io.ktor.server.response.respond
+
+fun Application.configureStatusPages() {
+  install(StatusPages) {
+    exception<InvalidIdException> { call, cause ->
+      call.respond(HttpStatusCode.BadRequest, mapOf("error" to cause.message))
+    }
+
+    exception<ResourceNotFoundException> { call, cause ->
+      call.respond(HttpStatusCode.NotFound, mapOf("error" to cause.message))
+    }
+
+    exception<Throwable> { call, cause ->
+      call.respond(
+          HttpStatusCode.InternalServerError,
+          mapOf("error" to "An unknown error happened."),
+      )
+    }
+  }
+}
