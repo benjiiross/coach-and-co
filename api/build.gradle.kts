bc76526ba61d378946e87ccceb20be318e7e2483
Benjamin <60489646+benjiiross@users.noreply.github.com>
2026-02-05 21:57:34 +0100

    add mockk for testing

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -37,6 +37,7 @@ dependencies {
   implementation(libs.ktor.server.config.yaml)
   testImplementation(libs.ktor.server.test.host)
   testImplementation(libs.kotlin.test.junit)
+  testImplementation(libs.mockk)
 }
 
 ktor { development = true }
